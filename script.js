59ce8f948e376efc9e293cb4ccd70459a8d7fcb1
Naresh Kumar Suthar <150646833+naresh-sthr@users.noreply.github.com>
2025-05-19 14:49:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 let password = document.querySelector('#input-txt');
 const url = 'https://random-password-generator5.p.rapidapi.com/random-password/index.php?length=10&lower_case=true&upper_case=true&numbers=true&symbols=true';
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'f10a9955b5msha85578bc3aca352p11f23ejsn25f6072b7bf4',
-		'X-RapidAPI-Host': 'random-password-generator5.p.rapidapi.com'
-	}
+  method: 'GET',
+  headers: {
+    'x-rapidapi-key': 'f10a9955b5msha85578bc3aca352p11f23ejsn25f6072b7bf4',
+    'x-rapidapi-host': 'random-password-generator5.p.rapidapi.com'
+  }
 };
 
-async function main(){
-try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-  password.value = result.random_password;
-} catch (error) {
-	console.error(error);
-}
+async function main() {
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();  // Using .text() instead of .json()
+    password.value = result;  // Set the password input field with the result
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function copy(){
+function copy() {
   password.select();
   document.execCommand("copy");
 }
